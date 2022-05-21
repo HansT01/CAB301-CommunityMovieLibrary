2dda9a955b7f2b0fbc72e760386cffdf42189233
Hans Teh <n10530851@qut.edu.au>
2022-05-21 17:14:01 +1000

    removed old tests from program, changed formatting for console ui, used while loop instead of recursive functions

--- cab301/MovieLibraryGUI.cs
+++ cab301/MovieLibraryGUI.cs
@@ -10,156 +10,148 @@ namespace cab301
 
         public MovieLibraryGUI()
         {
-
+            while (MainMenu());
         }
 
-        public static void MainMenu()
+        private bool MainMenu()
         {
             Console.Clear();
-            string menuText = "============================================================\n"
-                + "Welcome to Community Library Movie DVD Management System\n"
-                + "============================================================\n"
-                + "========================Main Menu===========================\n"
-                + "1.Staff Login\n"
-                + "2.Member Login\n"
-                + "0.Exit\n"
-                + "Enter your choice ==> (1/2/0)\n";
-            Console.WriteLine(menuText);
+            Console.WriteLine(String.Join(
+                Environment.NewLine,
+                "============================================================",
+                "Welcome to Community Library Movie DVD Management System",
+                "============================================================",
+                " ",
+                "========================Main Menu===========================",
+                " ",
+                "1. Staff Login",
+                "2. Member Login",
+                "0. Exit",
+                " ",
+                "Enter your choice ==> (1/2/0)"
+            ));
 
             int input = Convert.ToInt32(Console.ReadLine());
-            bool showMenu = true;
-            while (showMenu)
+            switch (input)
             {
-                switch (input)
-                {
-                    case 1:
-                        StaffMenu();
-                        break;
-                    case 2:
-                        MemberMenu();
-                        break;
-                    case 0:
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        showMenu = false;
-                        break;
-                }
+                case 1:
+                    while (StaffMenu());
+                    return true;
+                case 2:
+                    while (MemberMenu());
+                    return true;
+                case 0:
+                    return false;
+                default:
+                    return true;
             }
-            MainMenu();
         }
 
-        public static void StaffMenu()
+        private bool StaffMenu()
         {
             Console.Clear();
-            string menuText = "========================Stff Menu==========================\n"
-                + "1.Add new DVDs of a new movie to the system\n"
-                + "2.Remove DVDs of a movie from the system\n"
-                + "3.Register a new member with the system\n"
-                + "4.Remove a resitered member from the system\n"
-                + "5.Remove a resitered member from the system\n"
-                + "6.Display all members who are currently renting a particular movie\n"
-                + "0.Return to the main menu\n"
-                + "Enter your choice ==> (1/2/3/4/5/6/0)\n";
-            Console.WriteLine(menuText);
+            Console.WriteLine(String.Join(
+                Environment.NewLine,
+                "========================Staff Menu==========================",
+                " ",
+                "1. Add new DVDs of a new movie to the system",
+                "2. Remove DVDs of a movie from the system",
+                "3. Register a new member with the system",
+                "4. Remove a resitered member from the system",
+                "5. Remove a resitered member from the system",
+                "6. Display all members who are currently renting a particular movie",
+                "0. Return to the main menu",
+                " ",
+                "Enter your choice ==> (1/2/3/4/5/6/0)"
+            ));
 
             int input = Convert.ToInt32(Console.ReadLine());
-            bool showMenu = true;
-            while (showMenu)
+            switch (input)
             {
-                switch (input)
-                {
-                    case 1:
-                        //Add new DVDs of a new movie to the system
-                        break;
-                    case 2:
-                        //Remove DVDs of a movie from the system
-                        break;
-                    case 3:
-                        //Register a new member with the system
-                        break;
-                    case 4:
-                        //Remove a resitered member from the system
-                        break;
-                    case 5:
-                        //Remove a resitered member from the system
-                        break;
-                    case 6:
-                        //Display all members who are currently renting a particular movie
-                        break;
-                    case 0:
-                        //Return to the main menu
-                        MainMenu();
-                        break;
-                    default:
-                        showMenu = false;
-                        break;
-                }
+                case 1:
+                    // Add new DVDs of a new movie to the system
+                    return true;
+                case 2:
+                    // Remove DVDs of a movie from the system
+                    return true;
+                case 3:
+                    // Register a new member with the system
+                    return true;
+                case 4:
+                    // Remove a resitered member from the system
+                    return true;
+                case 5:
+                    // Remove a resitered member from the system
+                    return true;
+                case 6:
+                    // Display all members who are currently renting a particular movie
+                    return true;
+                case 0:
+                    // Return to the main menu
+                    return false;
+                default:
+                    return true;
             }
-            StaffMenu();
         }
 
-        public static void MemberMenu()
+        private bool MemberMenu()
         {
             Console.Clear();
-            string menuText = "======================Member Menu==========================\n"
-                + "1.Browse all the movies\n"
-                + "2.Display all the information about a movie, given the title of the movie\n"
-                + "3.Borrow a movie DVD\n"
-                + "4.Return a movie DVD\n"
-                + "5.List current borrowing movies\n"
-                + "6.Display the top 3 movies rented by the members\n"
-                + "0.Return to the main menu\n"
-                + "Enter your choice ==> (1/2/3/4/5/6/0)\n";
-            Console.WriteLine(menuText);
+            Console.WriteLine(String.Join(
+                Environment.NewLine,
+                "======================Member Menu==========================",
+                " ",
+                "1. Browse all the movies",
+                "2. Display all the information about a movie, given the title of the movie",
+                "3. Borrow a movie DVD",
+                "4. Return a movie DVD",
+                "5. List current borrowing movies",
+                "6. Display the top 3 movies rented by the members",
+                "0. Return to the main menu",
+                " ",
+                "Enter your choice ==> (1/2/3/4/5/6/0)"
+            ));
 
             int input = Convert.ToInt32(Console.ReadLine());
-            bool showMenu = true;
-            while (showMenu)
+            switch (input)
             {
-                switch (input)
-                {
-                    case 1:
-                        //Browse all the movies
-                        break;
-                    case 2:
-                        //Display all the information about a movie, given the title of the movie
-                        break;
-                    case 3:
-                        //Borrow a movie DVD
-                        break;
-                    case 4:
-                        //Return a movie DVD
-                        break;
-                    case 5:
-                        //List current borrowing movies
-                        break;
-                    case 6:
-                        //Display the top 3 movies rented by the members
-                        break;
-                    case 0:
-                        //Return to the main menu
-                        MainMenu();
-                        break;
-                    default:
-                        showMenu = false;
-                        break;
-                }
+                case 1:
+                    // Browse all the movies
+                    return true;
+                case 2:
+                    // Display all the information about a movie, given the title of the movie
+                    return true;
+                case 3:
+                    // Borrow a movie DVD
+                    return true;
+                case 4:
+                    // Return a movie DVD
+                    return true;
+                case 5:
+                    // List current borrowing movies
+                    return true;
+                case 6:
+                    // Display the top 3 movies rented by the members
+                    return true;
+                case 0:
+                    // Return to the main menu
+                    return false;
+                default:
+                    return true;
             }
-            MemberMenu();
         }
 
-        //Verify if the user has correct the correct user name and password
-        //Pre-condition: 
-        //Post-condition: 
-        public static bool verifyUser(string userType)
+        // Verify if the user has correct the correct user name and password
+        // Pre-condition: 
+        // Post-condition: 
+        public bool VerifyUser(string userType)
         {
             if (userType == "staff")
             {
-                //username and the password for the staff are ‘staff’ and ‘today123’, respectively
+                // username and the password for the staff are ‘staff’ and ‘today123’, respectively
             } else
             {
-                //Registered members are verified using their first name, last name and a password
+                // Registered members are verified using their first name, last name and a password
             }
             return true;
         }
